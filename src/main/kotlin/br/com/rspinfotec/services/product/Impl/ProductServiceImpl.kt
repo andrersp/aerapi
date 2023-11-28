4e131789d4ac428de4ff1a232a5e729662209e16
rspandre <rsp.assistencia@gmail.com>
2023-11-28 08:09:07 -0300

    start test products

--- src/main/kotlin/br/com/rspinfotec/services/product/Impl/ProductServiceImpl.kt
+++ src/main/kotlin/br/com/rspinfotec/services/product/Impl/ProductServiceImpl.kt
@@ -3,13 +3,40 @@ package br.com.rspinfotec.services.product.Impl
 import br.com.rspinfotec.dtos.product.ProductDetailDTO
 import br.com.rspinfotec.dtos.product.ProductRequestDTO
 import br.com.rspinfotec.dtos.product.ProductResumeDTO
+import br.com.rspinfotec.entity.Product
+import br.com.rspinfotec.repository.CategoryRepository
+import br.com.rspinfotec.repository.ProductRepository
 import br.com.rspinfotec.services.product.ProductService
+import br.com.rspinfotec.shared.exceptions.ApiError
+import br.com.rspinfotec.shared.exceptions.ApiException
 import jakarta.inject.Singleton
 
 @Singleton
-class ProductServiceImpl : ProductService {
+class ProductServiceImpl(
+    private val productRepository: ProductRepository,
+    private val categoryRepository: CategoryRepository
+) : ProductService {
     override fun create(payload: ProductRequestDTO): ProductDetailDTO {
-        TODO("Not yet implemented")
+
+        val category =
+            categoryRepository.findById(payload.categoryId)
+                .orElseThrow { ApiException(ApiError.REGISTRY_NOT_FOUND, "category not found") }
+        val product = Product(
+            name = payload.name,
+            minimumStock = payload.minimumStock,
+            saleValue = payload.saleValue,
+            category = category
+        )
+        productRepository.save(product)
+
+        return ProductDetailDTO(
+            category = category.name,
+            id = product.id,
+            minimumStock = product.minimumStock,
+            name = product.name,
+            saleValue = product.saleValue
+        )
+
     }
 
     override fun getById(productId: Int): ProductDetailDTO {
---
+++ src/test/kotlin/br/com/rspinfotec/constants/ProductConstants.kt
@@ -0,0 +1,29 @@
+package br.com.rspinfotec.constants
+
+import br.com.rspinfotec.dtos.product.ProductRequestDTO
+import br.com.rspinfotec.entity.Product
+
+object ProductConstants {
+
+    val PRODUCT_REQUEST = ProductRequestDTO(
+        name = "product test",
+        saleValue = 12.50,
+        minimumStock = 10,
+        categoryId = 1
+    )
+    val PRODUCT_1 = Product(
+        id = 1,
+        name = "product test",
+        saleValue = 12.50,
+        minimumStock = 10,
+        category = CategoryConstants.CATEGORY_1
+    )
+
+    val PRODUCT_2 = Product(
+        id = 2,
+        name = "product test 2",
+        saleValue = 12.50,
+        minimumStock = 10,
+        category = CategoryConstants.CATEGORY_2
+    )
+}
